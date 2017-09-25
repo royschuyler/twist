61214828aa1c3573fd21bb05585f7610a983304a
royschuyler <royschuyler@yahoo.com>
2017-09-25 13:50:49 -0700

    base shell constructor built and logging

--- min.js
+++ min.js
@@ -1,224 +1,219 @@
-// //MATH******************************************************
-// function radians(degrees) {
-//   return degrees * Math.PI / 180;
-// };
+//MATH******************************************************
+function radians(degrees) {
+  return degrees * Math.PI / 180;
+};
 
-// function sqrt(n){
-//   return Math.sqrt(n)
-// }
+function sqrt(n){
+  return Math.sqrt(n)
+}
 
-// function square(x){
-//   return Math.pow(x,2)
-// }
+function square(x){
+  return Math.pow(x,2)
+}
 
-// function abs(n){
-//   return Math.abs(n)
-// }
+function abs(n){
+  return Math.abs(n)
+}
 
-// //TRIG******************************************************
-// function sin (x) {
-//   return Math.sin(x)
-// }
+//TRIG******************************************************
+function sin (x) {
+  return Math.sin(x)
+}
 
-// function cos (x) {
-//   return Math.cos(x)
-// }
+function cos (x) {
+  return Math.cos(x)
+}
 
-// function tan (x) {
-//   return Math.tan(x)
-// }
+function tan (x) {
+  return Math.tan(x)
+}
 
-// function asin (x) {
-//   return Math.asin(x)
-// }
+function asin (x) {
+  return Math.asin(x)
+}
 
-// function acos (x) {
-//   return Math.cos(x)
-// }
+function acos (x) {
+  return Math.cos(x)
+}
 
-// function atan (x) {
-//   return Math.atan(x)
-// }
+function atan (x) {
+  return Math.atan(x)
+}
 
-// function cot (x) {
-//   return 1/Math.tan(x)
-// }
-
-
-// //********************************************************************
-// function split1(d,ax,ay,bx,by){
-//   var m = ((ay-by)/(ax-bx));
-//   var b = ay-(m*ax);
-//   var xUse = (ax-bx)/d;
-//   var obj = {
-//     x: [],
-//     y: []
-//   }
-//   for(i=0;i<=d;i++){
-//     var x = ax-(xUse*i);
-//     var y = (m*x)+b;
-//     obj.x.push(x);
-//     obj.y.push(y);
-//   }return obj
-// }
+function cot (x) {
+  return 1/Math.tan(x)
+}
 
 
-// function plotV(d,A,B,count,color){
-//   var smStr = '';
-//   for(i=0;i<d;i++){
-
-//     smStr += 'newbuffer ' + '</br>';
-//     smStr += 'addvalue ' + count + ' ' + A.x[i] + ' ' + A.y[i] + '</br>';
-//     smStr += 'addvalue ' + count + ' ' + B.x[i] + ' ' + B.y[i] + '</br>';
-
-//     smStr += 'colormode ' + 0 + '</br>';
-//     smStr += 'bcolor ' + color[0] + ' ' + color[1] + ' ' + color[2] + ' ' + count + '</br>';
-//     count += 1
-//   }
-//   return smStr
-// }
-
-// function multipleStart(H1,H2,move,base){
-//   text += '</br>' + 'new' + '</br>' + 'newbuffer ' + '</br>';
-//   var start = regulate(base);
-//   var pointsA = getPoints(d,start,H1,F,L);
-//   var pointsB = getPoints(d,start,H2,F,L);
-//   text += makeOne(count,d,pointsA,pointsB,start,H1,H2);
-//   //base += move;
-//   count+=1
-// //count += d*3
-// }
-
-// function multiple(H1,H2,move,base){
-//   text += '</br>' + '</br>' + 'newbuffer ' + '</br>';
-//   var start = regulate(base);
-//   var pointsA = getPoints(d,start,H1,F,L);
-//   var pointsB = getPoints(d,start,H2,F,L);
-//   text += makeOne(count,d,pointsA,pointsB,start,H1,H2);
-//   //base += move;
-//   count+=1
-// //count += d*3
-// }
-
-// function multipleEnd(H1,H2,move,base){
-//   text += '</br>' + 'newbuffer' + '</br>';
-//   var start = regulate(base);
-//   var pointsA = getPoints(d,start,H1,F,L);
-//   var pointsB = getPoints(d,start,H2,F,L);
-//   text += makeOne(count,d,pointsA,pointsB,start,H1,H2);
-//   var loc = '</br>' + 'savejpg /Users/royschuyler/Desktop/auto8/' + z + '.jpg 2' + '</br>' + 'close' + '</br>';
-//   var extra = 'windowsize 700 700' + '</br>' + 'blinewidth 2 all' + '</br>' + 'drawframe no' + '</br>' + 'asetticks x no' + '</br>' + 'asetticks y no' + '</br>' + 'asetminticks x no' + '</br>' + 'asetminticks y no' + '</br>' +'framewidth 0' + '</br>' + 'bstyle yes no no no no no no yes no no 0' + '</br>' + 'margins 0 0 0 0' + '</br>' + 'range x '  + -plotSize + ' ' + plotSize + '</br>' + 'range y ' + -plotSize + ' ' + plotSize;
-//   text += extra
-//   text += loc;
-//   //base += move;
-//   count+=1
-// }
-
-// function regulateArr(d,start){
-//   var arr = [];
-//   for(i=0;i<d;i++){
-//     if(start*i<=360){
-//       arr.push(start*i);
-//     }
-//     if(start*i>360){
-//       var multiplier = Math.floor((start*i/360) % 360);
-//       arr.push((start*i)-(360*multiplier))
-//     }
-//   }return arr
-// }
-
-// function regulate(start){
-//   var real;
-//   if(start<=360){
-//     real = start;
-//   }else {
-//     var multiplier = Math.floor((start/360) % 360);
-//     real = start-(360*multiplier)
-//   }return real
-// }
+//********************************************************************
+function split1(d,ax,ay,bx,by){
+  var m = ((ay-by)/(ax-bx));
+  var b = ay-(m*ax);
+  var xUse = (ax-bx)/d;
+  var obj = {
+    x: [],
+    y: []
+  }
+  for(i=0;i<=d;i++){
+    var x = ax-(xUse*i);
+    var y = (m*x)+b;
+    obj.x.push(x);
+    obj.y.push(y);
+  }return obj
+}
 
 
+function plotV(d,A,B,count,color){
+  var smStr = '';
+  for(i=0;i<d;i++){
 
-// //********************************************************************
+    smStr += 'newbuffer ' + '</br>';
+    smStr += 'addvalue ' + count + ' ' + A.x[i] + ' ' + A.y[i] + '</br>';
+    smStr += 'addvalue ' + count + ' ' + B.x[i] + ' ' + B.y[i] + '</br>';
 
-// function getPoints(d,start,H,F,L){
-//   var use = start;
-//   var p1 = radians(use);
-//   var p2 = radians(use + 90);
-//   var p3 = radians(use + 180);
-//   var p4 = radians(use + 270);
+    smStr += 'colormode ' + 0 + '</br>';
+    smStr += 'bcolor ' + color[0] + ' ' + color[1] + ' ' + color[2] + ' ' + count + '</br>';
+    count += 1
+  }
+  return smStr
+}
 
-//   var M = -(H/F);
-//   var t1 = atan(M);
-//   var t2 = atan((((M*L)+H))/(F+L));
-//   var H2 = tan(t2)*F;
-//   var a = L/(H-H2);
-//   var e = 1/a;
-//   var c = H-(L*e);
+function multipleStart(H1,H2,move,base){
+  text += '</br>' + 'new' + '</br>' + 'newbuffer ' + '</br>';
+  var start = regulate(base);
+  var pointsA = getPoints(d,start,H1,F,L);
+  var pointsB = getPoints(d,start,H2,F,L);
+  text += makeOne(count,d,pointsA,pointsB,start,H1,H2);
+  //base += move;
+  count+=1
+//count += d*3
+}
 
-//   var obj = {
-//     a: a,
-//     e: e,
-//     c: c,
-//     p1: {
-//       x: sin(p1)*L,
-//       y: (L*e*cos(p1))+c
-//     },
-//     p2: {
-//       x: sin(p2)*L,
-//       y: (L*e*cos(p2))+c
-//     },
-//     p3: {
-//       x: sin(p3)*L,
-//       y: (L*e*cos(p3))+c
-//     },
-//     p4: {
-//       x: sin(p4)*L,
-//       y: (L*e*cos(p4))+c
-//     },
-//     objA:{
-//       x: [],
-//       y: []
-//     },
-//     objB:{
-//       x: [],
-//       y: []
-//     },
-//     objC:{
-//       x: [],
-//       y: []
-//     },
-//     objD:{
-//       x: [],
-//       y: []
-//     },
-//     reverse: {
-//       objC:{
-//         x: [],
-//         y: []
-//       },
-//       objD:{
-//         x: [],
-//         y: []
-//       }
-//     }
-//   }
+function multiple(H1,H2,move,base){
+  text += '</br>' + '</br>' + 'newbuffer ' + '</br>';
+  var start = regulate(base);
+  var pointsA = getPoints(d,start,H1,F,L);
+  var pointsB = getPoints(d,start,H2,F,L);
+  text += makeOne(count,d,pointsA,pointsB,start,H1,H2);
+  //base += move;
+  count+=1
+//count += d*3
+}
 
-//   obj.objA.x = split1(d,obj.p1.x,obj.p1.y,obj.p2.x,obj.p2.y).x;
-//   obj.objA.y = split1(d,obj.p1.x,obj.p1.y,obj.p2.x,obj.p2.y).y;
-//   obj.objB.x = split1(d,obj.p2.x,obj.p2.y,obj.p3.x,obj.p3.y).x;
-//   obj.objB.y = split1(d,obj.p2.x,obj.p2.y,obj.p3.x,obj.p3.y).y;
-//   obj.objC.x = split1(d,obj.p3.x,obj.p3.y,obj.p4.x,obj.p4.y).x;
-//   obj.objC.y = split1(d,obj.p3.x,obj.p3.y,obj.p4.x,obj.p4.y).y;
-//   obj.objD.x = split1(d,obj.p4.x,obj.p4.y,obj.p1.x,obj.p1.y).x;
-//   obj.objD.y = split1(d,obj.p4.x,obj.p4.y,obj.p1.x,obj.p1.y).y;
-//   obj.reverse.objC.x = obj.objC.x.reverse();
-//   obj.reverse.objC.y = obj.objC.y.reverse();
-//   obj.reverse.objD.x = obj.objD.x.reverse();
-//   obj.reverse.objD.y = obj.objD.y.reverse();
+function multipleEnd(H1,H2,move,base){
+  text += '</br>' + 'newbuffer' + '</br>';
+  var start = regulate(base);
+  var pointsA = getPoints(d,start,H1,F,L);
+  var pointsB = getPoints(d,start,H2,F,L);
+  text += makeOne(count,d,pointsA,pointsB,start,H1,H2);
+  var loc = '</br>' + 'savejpg /Users/royschuyler/Desktop/auto8/' + z + '.jpg 2' + '</br>' + 'close' + '</br>';
+  var extra = 'windowsize 700 700' + '</br>' + 'blinewidth 2 all' + '</br>' + 'drawframe no' + '</br>' + 'asetticks x no' + '</br>' + 'asetticks y no' + '</br>' + 'asetminticks x no' + '</br>' + 'asetminticks y no' + '</br>' +'framewidth 0' + '</br>' + 'bstyle yes no no no no no no yes no no 0' + '</br>' + 'margins 0 0 0 0' + '</br>' + 'range x '  + -plotSize + ' ' + plotSize + '</br>' + 'range y ' + -plotSize + ' ' + plotSize;
+  text += extra
+  text += loc;
+  //base += move;
+  count+=1
+}
 
-//   return obj
-// }
+function regulateArr(d,start){
+  var arr = [];
+  for(i=0;i<d;i++){
+    if(start*i<=360){
+      arr.push(start*i);
+    }
+    if(start*i>360){
+      var multiplier = Math.floor((start*i/360) % 360);
+      arr.push((start*i)-(360*multiplier))
+    }
+  }return arr
+}
+
+function regulate(start){
+  var real;
+  if(start<=360){
+    real = start;
+  }else {
+    var multiplier = Math.floor((start/360) % 360);
+    real = start-(360*multiplier)
+  }return real
+}
+
+//********************************************************************
+
+
+function shell(d,start,H,F,L){
+  this.d = d;
+  this.start = start;
+  this.H = H;
+  this.F = F;
+  this.L = L;
+  this.M = -(H/F);
+  this.r1 = radians(start);
+  this.r2 = radians(start+90);
+  this.r3 = radians(start+180);
+  this.r4 = radians(start+270);
+  this.t1 = atan(this.M);
+  this.t2 = atan((((this.M*this.L)+this.H))/(this.F+this.L));
+  this.H2 = tan(this.t2)*this.F;
+  this.e = 1/(this.L/(this.H-this.H2));
+  this.c = this.H-(this.L*this.e);
+  this.p1 = {
+    x: sin(this.r1)*this.L,
+    y: (this.L*this.e*cos(this.r1))+this.c
+  };
+  this.p2 = {
+    x: sin(this.r2)*this.L,
+    y: (this.L*this.e*cos(this.r2))+this.c
+  };
+  this.p3 = {
+    x: sin(this.r3)*this.L,
+    y: (this.L*this.e*cos(this.r3))+this.c
+  };
+  this.p4 = {
+    x: sin(this.r4)*this.L,
+    y: (this.L*this.e*cos(this.r4))+this.c
+  };
+  this.A = {
+    x: split1(this.d,this.p1.x,this.p1.y,this.p2.x,this.p2.y).x,
+    y: split1(this.d,this.p1.x,this.p1.y,this.p2.x,this.p2.y).y
+  },
+  this.B = {
+    x: split1(this.d,this.p2.x,this.p2.y,this.p3.x,this.p3.y).x,
+    y: split1(this.d,this.p2.x,this.p2.y,this.p3.x,this.p3.y).y
+  },
+  this.C = {
+    x: split1(this.d,this.p3.x,this.p3.y,this.p4.x,this.p4.y).x,
+    y: split1(this.d,this.p3.x,this.p3.y,this.p4.x,this.p4.y).y
+  },
+  this.D = {
+    x: split1(this.d,this.p4.x,this.p4.y,this.p1.x,this.p1.y).x,
+    y: split1(this.d,this.p4.x,this.p4.y,this.p1.x,this.p1.y).y
+  },
+  this.reverse = {
+    A: {
+      x: this.A.x.reverse(),
+      y: this.A.y.reverse()
+    },
+    B: {
+      x: this.B.x.reverse(),
+      y: this.B.y.reverse()
+    },
+    C: {
+      x: this.C.x.reverse(),
+      y: this.C.y.reverse()
+    },
+    D: {
+      x: this.D.x.reverse(),
+      y: this.D.y.reverse()
+    }
+  }
+}
+
+
+var first = new shell(10,1,5,20,1);
+var second = new shell(10,1,4.5,20,1);
+console.log(first);
+console.log(second)
+
 
 // function makeOne(count,d,pointsA,pointsB,start,H,H2){
 
